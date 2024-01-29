7c0f674ef9109585c9f6a4c2898b4af8a9fd68ec
Caribla <caribla4@gmail.com>
2024-01-29 16:43:44 +0100

    Fixing CSS px measurements, removing overflow and burger JS commented

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,7 +1,10 @@
+// We find the burger element to add an event to it
 var burgerMenu = document.getElementById('burgerIcon');
 
+//We find the element that we want to trigger with the burger element
 var overlay = document.getElementById('burgerMenu');
 
+// Listener event type onClick, to toggle the visibility of the element overlay
 burgerMenu.addEventListener('click', function() {
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
